bdf3aa62e8a5efdc56ba84235ebd64bb1d4b891a
Nayim hossen <nayimh2015@gmail.com>
2022-05-14 03:21:05 +0600

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import './App.css';
+import Navbar from './Shared/Navbar';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Services from './Pages/Services';
+import Contact from './Pages/Contact';
+import Login from './Pages/Login';
 
 function App() {
   return (
     <div>
-
+      <Navbar>
+        <Home />
+        <About />
+        <Services />
+        <Contact />
+        <Login />
+      </Navbar>
     </div>
   );
 }
